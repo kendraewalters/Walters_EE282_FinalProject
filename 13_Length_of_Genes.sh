d49aea53b1e6eb5cc67db9df154c408929d6b90c
Kendra Walters <kewalter@uci.edu>
2016-12-11 01:40:08 +0000

    Added the plots and added load modules to 13/14 scripts

--- 13_Length_of_Genes.sh
+++ 13_Length_of_Genes.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+module load jje/kent/2014.02.19 2> /dev/null
+module load jje/jjeutils/0.1a 2> /dev/null
+
 bioawk -cgff ' $feature=="gene" {print $end - $start + 1} ' dmel-all-r6-13.gtf > length_of_genes.txt
 
 ./length_of_genes length_of_genes.txt
--- 14_Length_of_Exons.sh
+++ 14_Length_of_Exons.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+module load jje/kent/2014.02.19 2> /dev/null
+module load jje/jjeutils/0.1a 2> /dev/null
+
 bioawk -cgff ' $feature=="exon" { print $end - $start + 1 } ' dmel-all-r6-13.gtf > length_of_exons.txt
 
 ./length_of_exons length_of_exons.txt
